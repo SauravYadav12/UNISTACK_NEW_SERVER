34fa016572340f909757075ed6af02793edd1742
anurag-unicodez <anurag.dhurwey@unicodez.com>
2025-03-04 23:40:32 +0530

    gcp storage configuration done

--- controllers/storageController.ts
+++ controllers/storageController.ts
@@ -5,16 +5,20 @@ import {
 } from "@aws-sdk/client-s3";
 import { Request, Response } from "express";
 import dotenv from "dotenv";
+import { Storage } from "@google-cloud/storage";
+
 dotenv.config({ path: "./config.env" });
 const endpoint = process.env.S3CLIENT_END_POINT;
 const accessKeyId = process.env.STORAGE_ACCESS_KEY_ID as string;
 const secretAccessKey = process.env.STORAGE_SECRET_ACCESS_KEY as string;
 const bucket = process.env.STORAGE_BUCKET;
+const gcpBucket = process.env.GCP_STORAGE_BUCKET;
 
-if (!accessKeyId || !secretAccessKey || !bucket || !endpoint) {
+if (!accessKeyId || !secretAccessKey || !bucket || !endpoint || !gcpBucket) {
   console.log("invalid storage configuration : ", "Invalid Keys");
-  console.log({ accessKeyId, secretAccessKey, bucket, endpoint });
+  console.log({ accessKeyId, secretAccessKey, bucket, endpoint, gcpBucket });
 }
+
 const s3Client = new S3Client({
   endpoint,
   forcePathStyle: true,
@@ -24,6 +28,8 @@ const s3Client = new S3Client({
     secretAccessKey,
   },
 });
+
+const gcpStorage = new Storage();
 
 export const uploadFile = async (req: Request, res: Response) => {
   if (!req.file) {
@@ -46,6 +52,30 @@ export const uploadFile = async (req: Request, res: Response) => {
       ? endpoint.slice(0, -1)
       : endpoint;
     const url = `${mainEndPoint}/unistack-storage/${key}`;
+    res.json({
+      data: {
+        url,
+      },
+    });
+  } catch (err) {
+    console.error("Upload error:", err);
+    res.status(500).json({ error: err });
+  }
+};
+
+export const uploadFileToGcpStorage = async (req: Request, res: Response) => {
+  if (!req.file) {
+    res.status(400).json({ error: "No file to uploaded" });
+    return;
+  }
+
+  try {
+    const key = `uploads/${Date.now()}-${req.file.originalname}`;
+    await gcpStorage
+      .bucket(gcpBucket || "")
+      .file(key)
+      .save(req.file.buffer);
+    const url = `https://storage.googleapis.com/${gcpBucket}/${key}`;
     res.json({
       data: {
         url,
